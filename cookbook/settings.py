db67f27017657cc895e1e91d0cc34f38a0db8390
elenaterehova <tterekhovaa@mail.ru>
2024-01-28 17:03:02 +0300

    edit css, home page and settings

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jq2%&9_0491xzot7a$%=#r!l_4bn0x*)f!jv*m0u4u$s75$z1^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
