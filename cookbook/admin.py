fee247f7e88b0bbc58ebf2ca46723a36a3f7e63f
elenaterehova <tterekhovaa@mail.ru>
2024-02-03 14:18:18 +0300

    add models and admin panel

--- cookbook/admin.py
+++ cookbook/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Recipe, Product, RecipeIngredient
+from .models import (Recipe, RecipeIngredients, Product)
 
-admin.site.register(Recipe)
-admin.site.register(Product)
-admin.site.register(RecipeIngredient)
+admin.site.register(RecipeIngredients)
+
+
+class ProductVariantInline(admin.TabularInline):
+    model = RecipeIngredients
+    readonly_fields = ('id',)
+    extra = 0
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [ProductVariantInline]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'recipes_count')
--- cookbook/models.py
+++ cookbook/models.py
@@ -1,30 +1,40 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    recipes_count = models.IntegerField()
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    recipes_count = models.IntegerField(verbose_name='Количество использования', default=0)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return self.name
-
-
-class RecipeIngredient(models.Model):
-    ingredient = models.ForeignKey(Product, models.CASCADE)
-    recipe = models.ForeignKey("Recipe", models.CASCADE)
-    amount = models.IntegerField()
-    unit_of_measure = models.CharField(blank=True, max_length=20)
-
-    def __str__(self):
-        return str(self.ingredient) + " " + str(self.amount)
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=200)
-    ingredients = models.ManyToManyField(Product, through=RecipeIngredient)
+    title = models.CharField(max_length=150, default="", verbose_name='Название рецепта')
+    ingredients = models.ManyToManyField(Product, through='RecipeIngredients')
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
 
     def __str__(self):
-        return self.name
+        return self.title
+
+
+class RecipeIngredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, verbose_name="Рецепт")
+    ingredient = models.ForeignKey(Product, on_delete=models.CASCADE, default="", verbose_name='Ингредиент')
+    amount = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    unit_of_measure = models.CharField(default='г', max_length=10, verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.ingredient.name
 
